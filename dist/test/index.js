8b60b92e6780f9f89fd844da0c20bb2f1813b37c
kingzez <kingzez@sina.com>
2018-09-25 16:02:28 +0800

    ✨ Feat: version 1.0.0

---
+++ dist/test/index.js
@@ -0,0 +1,43 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ava_1 = __importDefault(require("ava"));
+const src_1 = require("../src");
+ava_1.default('add', (t) => {
+    const result = src_1.add(0.1, 0.2);
+    console.log(`0.1 + 0.2 = ${result}`);
+    t.is(result, 0.3);
+});
+ava_1.default('multiply', (t) => {
+    const result = src_1.multiply(0.3, 0.75);
+    console.log(`0.3 * 0.75 = ${result}`);
+    t.is(result, 0.225);
+});
+ava_1.default('subtract', (t) => {
+    const result = src_1.subtract(1.5, 1.2);
+    console.log(`1.5 - 1.2 = ${result}`);
+    t.is(result, 0.3);
+});
+ava_1.default('divide', (t) => {
+    const result = src_1.divide(0.3, 0.1);
+    console.log(`0.3 - 0.1 = ${result}`);
+    t.is(result, 3);
+});
+ava_1.default('num.add', (t) => {
+    const result = 0.1.add(0.2);
+    t.is(result, 0.3);
+});
+ava_1.default('num.mul', (t) => {
+    const result = 0.3.mul(0.75);
+    t.is(result, 0.225);
+});
+ava_1.default('num.sub', (t) => {
+    const result = 1.5.sub(1.2);
+    t.is(result, 0.3);
+});
+ava_1.default('num.div', (t) => {
+    const result = 0.3.div(0.1);
+    t.is(result, 3);
+});
---
+++ test/index.ts
@@ -0,0 +1,46 @@
+import test from 'ava'
+import  { add, multiply, subtract, divide } from '../src'
+
+test('add', (t) => {
+    const result = add(0.1, 0.2)
+    console.log(`0.1 + 0.2 = ${result}`)
+    t.is(result, 0.3)
+})
+
+test('multiply', (t) => {
+    const result = multiply(0.3, 0.75)
+    console.log(`0.3 * 0.75 = ${result}`)
+    t.is(result, 0.225)
+})
+
+test('subtract', (t) => {
+    const result = subtract(1.5, 1.2)
+    console.log(`1.5 - 1.2 = ${result}`)
+    t.is(result, 0.3)
+})
+
+test('divide', (t) => {
+    const result = divide(0.3, 0.1)
+    console.log(`0.3 - 0.1 = ${result}`)
+    t.is(result, 3)
+})
+
+test('num.add', (t) => {
+    const result = 0.1.add(0.2)
+    t.is(result, 0.3)
+})
+
+test('num.mul', (t) => {
+    const result = 0.3.mul(0.75)
+    t.is(result, 0.225)
+})
+
+test('num.sub', (t) => {
+    const result = 1.5.sub(1.2)
+    t.is(result, 0.3)
+})
+
+test('num.div', (t) => {
+    const result = 0.3.div(0.1)
+    t.is(result, 3)
+})
